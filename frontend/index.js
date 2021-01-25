edbce26286b74182db240a7e6211ecde8df0ad29
Kat Leight <leightkt@gmail.com>
2021-01-25 15:26:08 -0700

    wrapped add options in a function

--- frontend/index.js
+++ frontend/index.js
@@ -4,10 +4,13 @@ const $activitySelect = document.querySelector("#activity-type")
 const $activityDisplay = document.querySelector("#display-activity")
 const $signInForm = document.querySelector("#sign-in-form")
 
+function setActivityOptions(){
+    activityTypes.forEach(activity => {
+        const $activityOption = document.createElement('option')
+        $activityOption.innerText = activity
+        $activitySelect.appendChild($activityOption)
+    })
+}
 
-activityTypes.forEach(activity => {
-    const $activityOption = document.createElement('option')
-    $activityOption.innerText = activity
-    $activitySelect.appendChild($activityOption)
-})
 
+setActivityOptions()

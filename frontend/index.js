458fef322d02cff3d77ceaf7fb89bda2ae636b20
Kat Leight <leightkt@gmail.com>
2021-01-25 15:24:26 -0700

    added user login form and get activity by type form

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,13 @@
-// console.log("hit js")
+const backendURL = 'http://localhost:9000'
+const activityTypes = ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
+const $activitySelect = document.querySelector("#activity-type")
+const $activityDisplay = document.querySelector("#display-activity")
+const $signInForm = document.querySelector("#sign-in-form")
+
+
+activityTypes.forEach(activity => {
+    const $activityOption = document.createElement('option')
+    $activityOption.innerText = activity
+    $activitySelect.appendChild($activityOption)
+})
+

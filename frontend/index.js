a8e00de08c2b9fe9c2a501841681321a658f8c05
Kat Leight <leightkt@gmail.com>
2021-01-26 11:04:45 -0700

    render random activity to index.html with fetch to getActvity route on activities controller

--- frontend/index.js
+++ frontend/index.js
@@ -3,6 +3,8 @@ const activityTypes = ["education", "recreational", "social", "diy", "charity", 
 const $activitySelect = document.querySelector("#activity-type")
 const $activityDisplay = document.querySelector("#display-activity")
 const $signInForm = document.querySelector("#sign-in-form")
+const queryParams = new URLSearchParams(window.location.search)
+const activityType = queryParams.get('type')
 
 function setActivityOptions(){
     activityTypes.forEach(activity => {
@@ -11,6 +13,30 @@ function setActivityOptions(){
         $activitySelect.appendChild($activityOption)
     })
 }
+
+if (activityType){
+    fetch(`${backendURL}/getActivity?type=${activityType}`)
+        .then(response => response.json())
+        .then(activity => {
+            displayActivity(activity)
+            setActivityOptions()
+        })
+}
+
+function displayActivity(activity){
+    $activityName = document.querySelector('#activity-name')
+    $activityType = document.querySelector('#activity-type')
+    $activityParticipants = document.querySelector('#participants')
+    $activityPrice = document.querySelector('#price')
+    $activityAccessibility = document.querySelector('#accessibility')
+
+    $activityName.textContent = `Activity: ${activity.activity}`
+    $activityType.textContent = `Type: ${activity.type}`
+    $activityParticipants.textContent = `Number of Participants: ${activity.participants}`
+    $activityPrice.textContent = `Price: ${activity.price}`
+    $activityAccessibility.textContent = `Accessbility Rating: ${activity.accessibility}`
+}
+
 
 
 setActivityOptions()

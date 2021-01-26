999b29f6a194b7537efdffad8002bd03b70168bb
Raz Al-jaf <razaljaf@gmail.com>
2021-01-26 15:09:06 -0700

    pseudocoded

--- frontend/showFavorites.js
+++ frontend/showFavorites.js
@@ -1,1 +1,25 @@
 const $favoritesContainer = document.querySelector("#display-favorites")
+
+fetch("")
+
+
+function save() {
+    Item = document.getElementById("input").value
+
+
+
+    localStorage.setItem("storedItem", Item)
+
+
+
+
+    localStorage.getItem("storedItem")
+
+
+
+
+    document.getElementById("savedText") = Item + "Added!"
+
+
+    document.getElementById(created-favorite) = storedItem + "here's a list of your favs!"
+}
